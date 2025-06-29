55693ac9cd6a21cffb761956c20d2ba7c79ad53b
pdk134 <1390204285@qq.com>
2025-06-29 19:30:51 +0800

    20250629

--- Training/src/VaribleDemo3.java
+++ Training/src/VaribleDemo3.java
@@ -1,20 +1,16 @@
-public class VaribleDemo3{
+import java.util.Scanner;
+
+public class VaribleDemo3 {
     //主入口
     public static void main(String[] args){
-      
-        String name = "黑马PDK";
-        System.out.println(name);
-    
-        int age = 20;
-         System.out.println(age);   
-
-        char gender = '男';
-        System.out.println(gender);  
-
-        double height = 178.8;
-        System.out.println(height);
-
-        boolean flag = true;
-        System.out.println(flag);
+        Scanner sc = new Scanner(System.in);
+        // 输入两个整数
+        System.out.println("请输入两个整数：");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        boolean result1 = a > b;
+        System.out.println(result1);
+        // 关闭Scanner对象
+        sc.close(); 
     }
 }
